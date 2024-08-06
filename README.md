4979870bca1c2c871f01085f7cd9794448fba7a5
shalluv <leon555thai@gmail.com>
2024-08-06 15:02:27 +0700

    feat: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Intania - Form
+
+Intania Form is a form wrapper of NocoDB for internal uses in the Engineering Faculty of Chulalongkorn University.
+
+## Contributing
+
+Please read the [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+import tailwindcssAnimate from 'tailwindcss-animate'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+const config: Config = {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
+}
+export default config
