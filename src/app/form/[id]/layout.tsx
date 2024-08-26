7120045a7e67c2506ed467419f0ac3a4e6638b35
neennera <neennera@gmail.com>
2024-08-27 01:09:38 +0700

    chore: rename file

--- src/app/form/[id]/layout.tsx
+++ src/app/form/[id]/layout.tsx
@@ -4,7 +4,6 @@ export default async function FormLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
-  params: { id: string }
 }>) {
   return (
     <div className='mx-auto flex w-full max-w-3xl flex-col items-center justify-center gap-8 py-8'>
