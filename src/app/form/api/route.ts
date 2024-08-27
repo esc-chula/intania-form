306ee8b3a124a5b43e357418e4ac2b60f8de7b98
neennera <neennera@gmail.com>
2024-08-27 11:17:40 +0700

    refactor: routh

--- src/app/form/api/route.ts
+++ src/app/form/api/route.ts
@@ -2,35 +2,37 @@ import { getNocoID } from '@/lib/fetcher'
 import { getFormSchema } from '@/lib/form/get-form-schema'
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const id = searchParams.get('id')
-  if (!id) {
+  // checking id
+  const id = checkIdParams(request)
+  if (id === null) {
     return Response.json({ error: 'ID not provided' }, { status: 400 })
   }
 
+  // get from nocodb
   const form = await getFormSchema(id)
   return Response.json(form)
 }
 
 export async function POST(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const id = searchParams.get('id')
-  if (!id) {
+  // checking id & body & form-name
+  const id = checkIdParams(request)
+  if (id === null) {
     return Response.json({ error: 'ID not provided' }, { status: 400 })
   }
 
   const body = await request.json()
-  const tableIdResponse = await getNocoID(id)
-  if (tableIdResponse == null) {
-    return Response.json({ error: 'No form in this name' }, { status: 400 })
-  }
-  const tableID = tableIdResponse.TableID
-
-  // return if any field is ""
-  if (Object.values(body).some((value) => value === '')) {
+  const isMissingField = await checkMissingField(body)
+  if (isMissingField) {
     return Response.json({ error: 'Missing Field' }, { status: 400 })
   }
 
+  const tableIdResponse = await getNocoID(id)
+  if (tableIdResponse === null) {
+    return Response.json({ error: 'No matching form name' }, { status: 400 })
+  }
+  const tableID = tableIdResponse.TableID
+
+  // post to nocodb
   const response = await fetch(
     `${process.env.NOCODB_BASE_URL}/api/v2/tables/${tableID}/records`,
     {
@@ -43,10 +45,26 @@ export async function POST(request: Request) {
     }
   )
 
+  // sent back error message (if any)
   if (!response.ok) {
     const errorMsg = await response.json()
     return Response.json({ error: errorMsg.msg }, { status: response.status })
   }
-
   return Response.json(await response.json())
+}
+
+const checkIdParams = (request: Request) => {
+  const { searchParams } = new URL(request.url)
+  const id = searchParams.get('id')
+  if (!id) {
+    return null
+  }
+  return id
+}
+
+const checkMissingField = async (body: Promise<Response>) => {
+  if (Object.values(body).some((value) => value === '')) {
+    return true
+  }
+  return false
 }
