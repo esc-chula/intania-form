11e3984195bda585ad2ee464eee6c472948d8a07
Yiwahpsp <Patsaporn.Kaewkong@gmail.com>
2024-10-03 12:06:55 +0700

    fix: layout responsive | add: tailwind config, form container, reset button

--- src/app/form/[id]/layout.tsx
+++ src/app/form/[id]/layout.tsx
@@ -5,5 +5,9 @@ export default async function FormLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return <FormProvider>{children}</FormProvider>
+  return (
+    <div className='flex size-full flex-col items-center justify-start gap-8 px-5 py-8'>
+      <FormProvider>{children}</FormProvider>
+    </div>
+  )
 }
