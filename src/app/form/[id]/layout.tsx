6301274966625b4fc85e3db839557e92f137252d
chonlaprim <primsrisanan@gmail.com>
2024-10-04 00:16:30 +0700

    parent 7556f5c275719274c6df530986f87a974ad44834 author chonlaprim <primsrisanan@gmail.com> 1727975790 +0700 committer shalluv <leon555thai@gmail.com> 1728629269 +0700
    style: required-mark
    
    fix: include required in get API rrespond
    
    chore: delete extra log
    
    chore: resolve merge
    
    fix: fix geting required api
    
    feat: error mes on input
    
    style: update required-mark
    
    refactor: clean up a bit

--- src/app/form/[id]/layout.tsx
+++ src/app/form/[id]/layout.tsx
@@ -5,9 +5,5 @@ export default async function FormLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return (
-    <div className='flex size-full flex-col items-center justify-start gap-8 px-5 py-8'>
-      <FormProvider>{children}</FormProvider>
-    </div>
-  )
+  return <FormProvider>{children}</FormProvider>
 }
