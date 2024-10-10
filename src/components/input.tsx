0d778d09ba77f7a208ee9bf65398cb46b6568c5a
Yiwahpsp <Patsaporn.Kaewkong@gmail.com>
2024-10-10 19:21:09 +0700

    build: input component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import toast from 'react-hot-toast'
 
 import { cn } from '@/lib/utils'
-
 interface InputProps {
   id?: string
   name?: string
@@ -17,8 +16,6 @@ interface InputProps {
   className?: string
   disabled?: boolean
   description?: string
-  error?: boolean
-  errorMessage?: string
 }
 
 export function Input({
@@ -33,8 +30,6 @@ export function Input({
   className,
   disabled,
   description,
-  error,
-  errorMessage,
 }: InputProps) {
   const [isValid, setIsValid] = useState(false)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -45,44 +40,38 @@ export function Input({
   }
 
   return (
-    <div className='flex w-full flex-col gap-1'>
-      <input
-        id={id}
-        name={name}
-        type={type}
-        value={value}
-        onChange={handleChange}
-        required={required}
-        pattern={pattern}
-        placeholder={placeholder || 'คำตอบ'}
-        onInvalid={(e) => {
-          e.preventDefault()
-          setIsValid(true)
-          toast.error('กรุณากรอกคำตอบให้ครบ', { id: 'invalid' })
-        }}
-        className={cn(
-          'flex w-full rounded-md border border-default border-neutral-200 px-2 py-1 text-body-1 outline-none transition-all duration-200 focus:border',
-          {
-            'border-red-600': error,
-            'focus:border-neutral-600': !error,
-          },
-          className
-        )}
-        disabled={disabled}
-      />
-      <p
-        className={cn(
-          'text-body-2 italic text-red-600 transition-all duration-200',
-          {
-            'text-red-600': error,
-            'text-neutral-400': !error,
-          }
-        )}
-      >
-        {error ? errorMessage : description}
-      </p>
+    <div className='relative flex w-full'>
+      <div className='flex w-full flex-col gap-1'>
+        <input
+          id={id}
+          name={name}
+          type={type}
+          value={value}
+          onChange={handleChange}
+          required={required}
+          pattern={pattern}
+          placeholder={placeholder || 'คำตอบ'}
+          onInvalid={(e) => {
+            e.preventDefault()
+            setIsValid(true)
+            toast.error('กรุณากรอกคำตอบให้ครบ', { id: 'invalid' })
+          }}
+          className={cn(
+            'flex w-full rounded-md border border-default border-neutral-200 px-2 py-1 outline-none transition-all duration-200 focus:border',
+            {
+              'border-red-600': isValid,
+              'focus:border-neutral-600': !isValid,
+            },
+            className
+          )}
+          disabled={disabled}
+        />
+        <p className='text-detail font-light text-neutral-400 transition-all duration-200'>
+          {description}
+        </p>
+      </div>
       {isValid && (
-        <p className='my-2 w-full text-sm italic text-red-600'>
+        <p className='absolute -bottom-8 w-full text-body-2 italic text-red-600'>
           จำเป็นต้องตอบคำถามนี้
         </p>
       )}
